f3ce9d1bb54202f298f995a45ed1993f052c7f85
Hinamii/ Amanda <105732005+Hinami145@users.noreply.github.com>
2023-11-16 10:48:54 -0300

    Add files via upload

--- Amanda 2B Alura Midi/aluramidi-curso-arquivos-iniciais/main.js
+++ Amanda 2B Alura Midi/aluramidi-curso-arquivos-iniciais/main.js
@@ -1,16 +1,25 @@
-function tocaSomPom(){
-  document.querySelector('#som_tecla_pom').play();
+function tocaSom(idElemntoAudio){
+  document.querySelector(idElemntoAudio).play();
   
 }
+document.querySelector('.tecla_pom').onclick = tocaSomPom;
+
 const listaDeTeclas= document.querySelectorAll('.tecla');
 
-let contador= 0;
-
-while (contador < 9){
-  listaDeTeclas[0]
-}
 
 //Essa função consegue selecionar e ler o arquivo, mas não conseguimos 
 //manipular o elemento
+let contador= 0;
 
-document.querySelector('.tecla_pom');
+while (contador < listaDeTeclas.length){
+  const efeito =listaDeTeclas[contador].class[1];
+ const idAudio = "#som_"+efeito;
+ listaDeTeclas[contador].onclick = function()
+  {
+    tocaSom(idAudio);
+}
+contador = contador + 1;
+console.log(contador);
+}
+
+  
